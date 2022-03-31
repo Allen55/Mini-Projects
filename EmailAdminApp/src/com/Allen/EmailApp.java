5c4c0defdec3cd55971645c91a90f1e7016ab668
Allen Harper <aharper1609@gmail.com>
2022-03-30 22:15:57 -0500

    Email Administration Application

---
+++ EmailAdminApp/src/com/Allen/EmailApp.java
@@ -0,0 +1,12 @@
+package com.Allen;
+
+public class EmailApp {
+
+    public static void main(String[] args) {
+
+        Email em1 = new Email("John", "Doe");
+
+        em1.showInfo();
+
+    }
+}
