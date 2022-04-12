061415f1f64dcf7eb4716f9b6024f20fe6bb3dcf
Allen Harper <aharper1609@gmail.com>
2022-04-11 23:18:13 -0500

    Update and Delete methods

--- restdemo/src/main/java/com/example/restdemo/topics/TopicController.java
+++ restdemo/src/main/java/com/example/restdemo/topics/TopicController.java
@@ -1,8 +1,7 @@
 package com.example.restdemo.topics;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,4 +16,27 @@ public class TopicController {
     public List<Topic> getAllTopics(){   // List<Topic> gets converted to JSON automatically in the http response
         return topicService.getAllTopics();
     }
+
+
+    @RequestMapping("/topics/{id}")
+    public Topic getTopic(@PathVariable String id){
+        return topicService.getTopic(id);
+    }
+
+    @PostMapping("/topics")                        // get the post body, convert it to a topic instance, and convert it to a list in the topic service
+    public void addTopic(@RequestBody Topic topic){  // taking request body and converting to topic instance
+        topicService.addTopic(topic);
+    }
+    @RequestMapping(method=RequestMethod.PUT, value="/topics/{id}")
+    public void updateTopic(@RequestBody Topic topic, @PathVariable String id){  // taking request body and converting to topic instance
+        topicService.updateTopic(id, topic);
+    }
+
+    // OR @RequestMapping(method=RequestMethod.DELETE, value="/topics/{id}")
+    @DeleteMapping("/topics/{id}")
+    public void deleteTopic(@PathVariable String id){  // taking request body and converting to topic instance
+        topicService.deleteTopic(id);
+    }
+
+
 }
